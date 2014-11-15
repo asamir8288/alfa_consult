4640fa33fdc865bd8196c297eb5e023f25334ae4
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-11-16 00:51:48 +0200

    page title

--- application/views/frontend/template.php
+++ application/views/frontend/template.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title><?php echo (isset($page_title)) ? $page_title : '';?></title>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>layout/css/alfa.css"/>
 </head>
 
